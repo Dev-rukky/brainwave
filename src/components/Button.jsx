e3e4b0d943e3eeebd46e4e70ae185ab70a338985
Rukky <abrukevwe@gmail.com>
2024-07-18 14:56:18 +0100

    Hero Done

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import ButtonSvg from '../assets/svg/ButtonSvg';
 
 const Button = ({ className, href, onClick, children, px, white }) => {
-    const classes = `button relative infline-flex items-center justify-center h-11 tranisition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''} `;
+    const classes = `button relative inline-flex items-center justify-center h-11 tranisition-colors hover:text-color-1 ${px || 'px-7'} ${white ? 'text-n-8' : 'text-n-1'} ${className || ''} `;
     const spanClasses = `relative z-10`
     const renderButton = () => (
         <button className={classes} onClick={onClick}>
