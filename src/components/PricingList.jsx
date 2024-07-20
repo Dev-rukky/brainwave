e5d93ae33a392b8ff51523eeb88d34a4fb54fdb2
Rukky <abrukevwe@gmail.com>
2024-07-20 16:52:35 +0100

    Bug fixed

--- src/components/PricingList.jsx
+++ src/components/PricingList.jsx
@@ -7,7 +7,7 @@ const PricingList = () => {
     return (
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-[1rem]">
             {pricing.map((item) => (
-                <div key={item.id} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[1rem] lg:w-auto even:py-14 odd:py-8 odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3">
+                <div key={item.id} className="w-[19rem] max-lg:w-full h-full px-6 bg-n-8 border border-n-6 rounded-[1rem] lg:w-auto even:py-14 odd:py-8 xl:odd:my-4 [&>h4]:first:text-color-2 [&>h4]:even:text-color-1 [&>h4]:last:text-color-3">
                     <h4 className="h4 mb-4">{item.title}</h4>
                     <p className="body-2 min-h-[4rem] mb-3 text-n-1/50">{item.description}</p>
                     <div className="flex items-center h-[5.5rem] mb-6">
