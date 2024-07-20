8a33be60b2b9efb2d3000d4f7ff8da50bce36257
Rukky <abrukevwe@gmail.com>
2024-07-20 17:05:06 +0100

    Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
+import { socials } from "../constants";
+import Section from "./Section";
 
 
 const Footer = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+    return (
+        <Section crosses className="!px-0 !py-10">
+            <div className="container flex items-center justify-center gap-10 max-sm:flex-col sm:justify-between">
+                <p className="caption text-n-4 lg:block">©{new Date().getFullYear()}. All rights reserved</p>
+                <ul className="flex gap-5 flex-wrap">
+                    {socials.map((item) => (
+                        <a key={item.id} href={item.url} target="_blank" className="flex items-center justify-center w-10 h-10 bg-n-7 border border-n-8/10 rounded-full transition-colors hover:bg-n-1/10">
+                            <img src={item.iconUrl} alt={item.title} width={16} height={16} />
+                        </a>
+                    ))}
+                </ul>
+            </div>
+        </Section>
+    );
 };
 
 export default Footer;
